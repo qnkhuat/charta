ed91768759adbcd8362fb11243c3f00a6ae1f1d8
Earther <qn.khuat@gmail.com>
2021-04-14 20:37:23 +0700

    usable

--- src/index.js
+++ src/index.js
@@ -1,68 +1,111 @@
+import './index.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 
-class Square extends React.Component {
-  render() {
+class TextArea extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      x: props.x,
+      y: props.y,
+      value: '',
+      rows: 3,
+      cols: 30,
+
+    };
+
+  }
+
+  handleChange(e){
+    const text = e.target.value;
+    const lines = text.split("\n");
+    const max_len = lines.reduce((a, b) => a.length > b.length ? a : b, '').length;
+
+
+    this.setState({
+      value: text,
+      rows: lines.length > 3 ? lines.length : 3,
+      cols: max_len > 30 ? max_len : 30
+    });
+  }
+
+  render(){
+    const style = {
+      position: 'absolute',
+      left: this.state.x + 'px',
+      top: this.state.y + 'px',
+      background:'transparent'
+    }
     return (
-      <button className="square">
-        {this.props.value}
-      </button>
-    );
+      <textarea style={style} autoFocus 
+        rows={this.state.rows}
+        cols={this.state.cols}
+        onChange={this.handleChange.bind(this)}/>  
+    )
+
   }
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+class Paper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: "bg-white",
+      divs: [],
+      x: 0,
+      y: 0,
+    };
+  }
+  
+  createTextArea(x, y){
+    return (
+        <TextArea key={`${x},${y}`} x={x} y={y}/>  
+    )
+  }
+
+  createDiv(e) {
+    const x = e.clientX,
+      y = e.clientY;
+    const divs = this.state.divs.slice();
+    const newdivs = [];
+    console.log(divs);
+    for (const div in divs){
+      console.log(div);
+    }
+    divs.push(this.createTextArea(x, y));
+    this.setState({divs:divs});
   }
 
   render() {
-    const status = 'Next player: X';
-
+    const divs = this.state.divs;
     return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+      <div 
+        className={`w-full h-full absolute top-0 left-0 ${this.state.color}`}
+        onClick={this.createDiv.bind(this)}
+      >
+        <div>
+          {divs.length !== 0 && divs.map((div)=> div)}
         </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+
       </div>
-    );
+    )
+
   }
 }
 
-class Game extends React.Component {
-  render() {
+class App extends React.Component {
+  render(){
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
+      <div id="wrapper">
+        <Paper/>
       </div>
-    );
+    )
   }
 }
-
-// ========================================
 
 ReactDOM.render(
-  <Game />,
+  <App/>,
   document.getElementById('root')
 );
 
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      width: {
+        'a4': '29.7cm', // 300dpi
+        'a3': '42cm',
+      },
+      height: {
+        'a4': '21cm',
+        'a3': '29.7cm'
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
