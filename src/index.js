63f0b96f5f94dddf3a303527705476cbd66e84c2
Earther <qn.khuat@gmail.com>
2021-04-15 16:15:21 +0700

    add option

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import ReactDOM from 'react-dom';
+import Draggable from 'react-draggable';
 import './index.css'
+import {
+    ControlledMenu,
+    Menu,
+    MenuItem,
+    MenuButton,
+    SubMenu
+} from '@szhsin/react-menu';
+import '@szhsin/react-menu/dist/index.css';
 
+import {SketchField, Tools} from 'react-sketch';
 
 function getTextWidth(text) {
   const canvas = document.createElement('canvas');
@@ -82,15 +92,27 @@ class Paper extends React.Component {
     super(props);
     this.state = {
       divs: [],
+      selectedMode: 'text',
+      isDragging: false,
+      menuOpen: false,
     };
+    this.options = [
+      {value: 'text', label: 'Text'},
+      {value: 'sketch', label: 'Sketch'},
+    ]
+    this.options = {
+      text: {label: '🖋', enable:true},
+      sketch: {label: '🎨', enable:false}
+    }
   }
+
 
   createDiv(x, y){
     const divs = this.state.divs.slice();
     const divRef = React.createRef();
     const div = <TextArea key={`${x},${y}`} x={x} y={y} ref={divRef}/>;
     const newDivs = divs.filter(div => div.ref.current.state.value.length > 0); // Remove non-text div
-    
+
     newDivs.push({div:div, ref:divRef}); // Add the current one
     this.setState({divs:newDivs});
   }
@@ -111,16 +133,80 @@ class Paper extends React.Component {
     }
   }
 
+  handleMenuClick(e){
+    if (this.state.isDragging){
+      this.setState({menuOpen: false})
+    } else {
+      this.setState({menuOpen: !this.state.menuOpen})
+    }
+  }
+
+  handleModeChange(e) {
+    this.setState({
+      selectedMode: e.value,
+      menuOpen: false,
+    })
+  }
+
+  handleDrag(){
+    this.setState({isDragging:true, menuOpen: false});
+  }
+
+  handleStopDrag(){
+    setTimeout(() => {
+      this.setState({isDragging:false});
+    }, 10) // stop is called before menuclick.
+  }
+
+
+
   render() {
     const intro = <h3 className="center absolute z-10 text-center text-gray-500 font-bold text-xl animate-pulse">This is a paper just like your paper<br></br>Click anywhere and type!</h3>;
     const divs = this.state.divs;
+
+    // *** Menu ***
+    const menuRef = React.createRef();
+    const menuButton = <button 
+      className='text-3xl rounded-full w-16 h-16 text-center bg-pink-300 focus:outline-none'
+      ref={menuRef} 
+      onClick={this.handleMenuClick.bind(this)}
+    >{this.options[this.state.selectedMode]['label']}</button>;
+    
     return (
-      <div 
-        className={`w-full h-full absolute top-0 left-0 bg-white`}
-        onClick={this.handleOnClick.bind(this)}
-      >
+      <div>
         {divs.length === 0 && intro}
-        <div id="texts">{divs.length !== 0 && divs.map((div)=> div.div)}</div>
+        <div className="absolute z-10">
+          <Draggable
+            onDrag={this.handleDrag.bind(this)}
+            onStop={this.handleStopDrag.bind(this)}
+            defaultPosition={{x:window.innerWidth-100, y:window.innerHeight-100}}
+          >
+            <div>
+              {menuButton}
+              <ControlledMenu
+                className='bg-opacity-0 shadow-none min-w-0 text-center'
+                anchorRef={menuRef}
+                direction='top'
+                isOpen={this.state.menuOpen}
+                onClick={this.handleModeChange.bind(this)}
+              >
+                {this.options.length !== 0 && Object.keys(this.options).map((key, index) =>
+                <MenuItem value={key} key={key} 
+                  className="p-0 rounded-full bg-pink-300 mt-2 hover:bg-blue-300 h-16">
+                  <p className="w-16 text-3xl inline-block text-center">{this.options[key]['label']}</p>
+                </MenuItem>
+                )}
+              </ControlledMenu>
+            </div>
+          </Draggable>
+        </div>
+
+        <div 
+          className={`w-full h-full bg-white absolute top-0 left-0`}
+          onClick={this.handleOnClick.bind(this)}
+        >
+          <div id="texts">{divs.length !== 0 && divs.map((div)=> div.div)}</div>
+        </div>
       </div>
     )
   }
@@ -129,8 +215,13 @@ class Paper extends React.Component {
 class App extends React.Component {
   render(){
     return (
-      <div id="wrapper ">
+      <div id="wrapper">
         <Paper/>
+        {/*<SketchField width='1024px'
+                         height='768px'
+                         tool={Tools.Pencil}
+                         lineColor='black'
+                         lineWidth={3}/>*/}
       </div>
     )
   }
@@ -140,4 +231,34 @@ ReactDOM.render(
   <App/>,
   document.getElementById('root')
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
